2941df79de623133c8320fc75f392c7c1cb9e7a8
diegohp <diego.blake11@gmail.com>
2023-01-16 21:16:05 -0600

    [ADD] join signup with frontend

--- src/index.js
+++ src/index.js
@@ -5,13 +5,25 @@ const {
   errorHandler,
   boomError,
 } = require('./middlewares/errors.middlewares');
+const cors = require('cors');
 
 const port = 3000;
-
 const app = express();
-
+const whitelist = ['http://127.0.0.1:3006', 'http://localhost:3006'];
+const options = {
+  origin: (origin, cb) => {
+    /*  console.log(origin); */
+    if (whitelist.includes(origin) || !origin) {
+      cb(null, true);
+    } else {
+      cb(new Error('Unauthorized'));
+    }
+  },
+};
 //app needs to use middleware express.json() to understand json
 app.use(express.json());
+app.use(cors(options));
+
 APIRouter(app);
 //middlewares to handle errors
 app.use(logErrors);
